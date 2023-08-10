21c28eb5d9e762755bf0d6d03344791eec0e0891
Madalena Campos <madacampos02@gmail.com>
2023-08-10 16:55:55 -0300

    estilizando historico e status

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,7 +1,48 @@
+import { HistoryContainer, HistoryList, Status } from "./styles";
+
 export function History() {
   return (
-    <div>
-      <h1>History</h1>
-    </div>
+    <HistoryContainer>
+      <h1>Meu hitórico</h1>
+
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th> Tarefa </th>
+              <th> Duração </th>
+              <th> Início </th>
+              <th> Status </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td> Estudar </td>
+              <td> 25 minutos </td>
+              <td> Há cerca de 2 meses </td>
+              <td>
+                <Status status="inprogress"> Em andamento </Status>
+              </td>
+            </tr>
+            <tr>
+              <td> Estudar </td>
+              <td> 25 minutos </td>
+              <td> Há cerca de 2 meses </td>
+              <td>
+                <Status status="done">Concluído</Status>
+              </td>
+            </tr>
+            <tr>
+              <td> Estudar </td>
+              <td> 25 minutos </td>
+              <td> Há cerca de 2 meses </td>
+              <td>
+                <Status status="cancel">Interrompido</Status>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
   );
 }
